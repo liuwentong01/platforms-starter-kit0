9b80afa12ef6b8526fd173a2faa1129e8dbdcd9b
liuwentong <liuwentong@kanyun.com>
2024-10-04 14:30:01 +0800

    fix: remove some tables

--- app/api/avatar/upload/route.ts
+++ app/api/avatar/upload/route.ts
@@ -1,20 +1,25 @@
 import { put } from "@vercel/blob";
 import { NextResponse } from "next/server";
+import { upload } from "@vercel/blob/client";
 
 export async function POST(request: Request): Promise<NextResponse> {
   const { searchParams } = new URL(request.url);
   const filename = searchParams.get("filename");
+  let blob;
 
   // ⚠️ The below code is for App Router Route Handlers only
-  const blob = await put(filename, request.body, {
-    access: "public",
-  });
+  if (filename && request.body) {
+    blob = await put(filename, request.body, {
+      access: "public",
+    });
+  } else {
+    throw new Error("Filename is null");
+  }
 
   // Here's the code for Pages API Routes:
   // const blob = await put(filename, request, {
   //   access: 'public',
   // });
-
   return NextResponse.json(blob);
 }
 
