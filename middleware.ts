63abf37347e59f8fb19129a3161454b86ef2e290
liuwentong <liuwentong@kanyun.com>
2024-10-05 13:46:28 +0800

    feat: 添加日志

--- middleware.ts
+++ middleware.ts
@@ -17,19 +17,20 @@ export const config = {
 // platforms-starter-kit0-m85q.vercel.app
 export default async function middleware(req: NextRequest) {
   const url = req.nextUrl;
-  console.log(`url: ${url}`);
 
   // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
   let hostname = req.headers
     .get("host")!
     .replace(".localhost:3000", `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
 
+  console.log(
+    `1000 Processing request for hostname: ${hostname} environment: ${process.env.NEXT_PUBLIC_VERCEL_DEPLOYMENT_SUFFIX}`,
+  );
   // special case for Vercel preview deployment URLs
   if (
     hostname.includes("---") &&
     hostname.endsWith(`.${process.env.NEXT_PUBLIC_VERCEL_DEPLOYMENT_SUFFIX}`)
   ) {
-    console.log(`00000000: ${hostname}`);
     hostname = `${hostname.split("---")[0]}.${
       process.env.NEXT_PUBLIC_ROOT_DOMAIN
     }`;
@@ -41,10 +42,13 @@ export default async function middleware(req: NextRequest) {
     searchParams.length > 0 ? `?${searchParams}` : ""
   }`;
 
+  console.log(
+    `2000 Processing request for path: ${path} ${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`,
+  );
+
   // rewrites for app pages
   if (hostname == `app.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) {
     const session = await getToken({ req });
-    console.log(`11111111: ${hostname}`);
     if (!session && path !== "/login") {
       return NextResponse.redirect(new URL("/login", req.url));
     } else if (session && path == "/login") {
@@ -57,27 +61,22 @@ export default async function middleware(req: NextRequest) {
 
   // special case for `vercel.pub` domain
   if (hostname === "vercel.pub") {
-    console.log(`22222222: ${hostname}`);
     return NextResponse.redirect(
       "https://vercel.com/blog/platforms-starter-kit",
     );
   }
+
+  console.log(`3000 hostname: ${new URL(`/${hostname}${path}`, req.url)}`);
 
   // rewrite root application to `/home` folder
   if (
     hostname === "localhost:3000" ||
     hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN
   ) {
-    console.log(
-      `33333333: ${hostname} path: ${path}, req.url: ${req.url}`,
-      "444",
-      JSON.stringify(new URL(`/home${path === "/" ? "" : path}`, req.url)),
-    );
     return NextResponse.rewrite(
       new URL(`/home${path === "/" ? "" : path}`, req.url),
     );
   } else {
-    console.log(`55555555: ${hostname} path: ${path}, req.url: ${req.url}`);
     return NextResponse.rewrite(
       new URL(`/home${path === "/" ? "" : path}`, req.url),
     );
